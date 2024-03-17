a8cda786fd4ba32bc5d327614459879834ecf1db
aakashks <114357454+aakashks@users.noreply.github.com>
2024-03-17 22:41:05 +0530

    now optimizers are subclass of torch.optim.optimizers enabling scheduler and more features

--- dp/optim.py
+++ dp/optim.py
@@ -1,18 +1,20 @@
 from abc import abstractmethod
 from typing import Literal, List, Dict
 import torch
+from torch.optim import Optimizer
 
 
-class DPOptimizer:
+class DPOptimizer(Optimizer):
     """
     Base class for all optimizers
-    note that this is an abstract class
+    Note that this is an abstract class
 
     Does Gradient Clipping (with per sample gradients)
     """
 
     def __init__(self, named_params, lr, max_grad_norm) -> None:
         self.named_params: Dict[str, torch.Tensor] = dict(named_params)
+        super().__init__(self.named_params.values(), {})
         self.lr = lr
         self.max_grad_norm = max_grad_norm
 
@@ -27,23 +29,10 @@ class DPOptimizer:
 
         return new_grads
 
-    @abstractmethod
-    def step(self, per_sample_grads) -> None:
-        raise NotImplementedError
-
-    @torch.no_grad()
-    def zero_grad(self) -> None:
-        for param in self.named_params.values():
-            if param.requires_grad:
-                param.grad = None
-
 
 class DPSGD(DPOptimizer):
     """
-    base class for SGD based dp optimizers
-    change modify_grad to accordingly add noise in the gradients
-
-    Note that here the gradient obtained is aggregated over all minibatch samples
+    Differentiable Private Stochastic Gradient Descent
     """
 
     def __init__(self, named_params, lot_size, lr=1e-3, noise_scale=4, max_grad_norm=4, weight_decay=0.) -> None:
@@ -51,7 +40,7 @@ class DPSGD(DPOptimizer):
         self.noise_std = noise_scale * max_grad_norm / lot_size
         self.wd = weight_decay
 
-    def add_noise(self, grad: torch.Tensor) -> torch.Tensor:
+    def _add_noise(self, grad: torch.Tensor) -> torch.Tensor:
         return grad + torch.normal(0, self.noise_std, grad.shape, device=grad.device)
 
     @torch.no_grad()
@@ -59,5 +48,5 @@ class DPSGD(DPOptimizer):
         clipped_grads = self._average_and_clip_grads(per_sample_grads)
         for name, param in self.named_params.items():
             if param.requires_grad and param.grad is not None:
-                g = self.add_noise(clipped_grads[name])
+                g = self._add_noise(clipped_grads[name])
                 param.copy_((1 - self.wd) * param.detach() - self.lr * g)
