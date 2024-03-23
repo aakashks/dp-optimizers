85a048be3c22ddc7646c74be8fa56fdac506ba68
aakashks <114357454+aakashks@users.noreply.github.com>
2024-03-24 01:42:34 +0530

    minor fix

--- dp/accountants.py
+++ dp/accountants.py
@@ -33,21 +33,21 @@ class ModifiedMomentsAccountant(MomentsAccountant):
     “Differentially Private Deep Learning with Iterative Gradient Descent Optimization.”
     ACM/IMS Transactions on Data Science 2, no. 4 (November 30, 2021): 1–27. https://doi.org/10.1145/3491254.
     """
-    def __init__(self, sigma, q, delta=1e-5):
+    def __init__(self, sigma, q, delta=1e-5, max_moment=32):
         super().__init__(sigma, q, delta)
-
+        self.max_moment = max_moment
         # ensure conditions from Theorem 3 are met
         assert sigma >= 1
         assert q < 1 / sigma
 
     def _compute_epsilon(self, T):
         # order of moments
-        lambda_ = min(32, 1 / (10 * self.q ** 2))  # lambda must be <= 32
+        lambda_ = min(self.max_moment, 1 / (10 * self.q ** 2))  # lambda must be <= 32
 
         # the cumulant generating function of the privacy loss at each step of the optimization
-        K_g = self.T * torch.log(torch.tensor(1 + (self.q ** 2 * lambda_ ** 2) / self.sigma ** 2))
+        K_g = T * torch.log(torch.tensor(1 + (self.q ** 2 * lambda_ ** 2) / self.sigma ** 2))
 
-        epsilon = 0.002 * self.T / lambda_
+        epsilon = 0.002 * T / lambda_
         assert epsilon > 0, "epsilon must be positive"
 
         return max(epsilon, 2 * K_g / lambda_)
